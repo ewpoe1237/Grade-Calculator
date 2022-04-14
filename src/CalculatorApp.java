9d0ab377346b63f6ea529e1ad3744d5e87b4ad66
Jenna <jennahofseth@hotmail.com>
2022-04-14 18:39:53 -0400

    finished up first card

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 
@@ -15,15 +16,20 @@ public class CalculatorApp {
     private JButton deleteCat;
     private JButton contToInput;
     private JPanel gradeInput;
+    private JLabel catSelected;
+    private JLabel weightSelected;
+    private JLabel totalWeight;
 
     static CalculatorApp myGUI = new CalculatorApp();
     static JFrame frame;
 
     static HashMap<String, Integer> noCapitals = new HashMap<>();
+    static ArrayList<Category> inputCategories = new ArrayList<>();;
 
     public static void main(String[] args) {
         frame = new JFrame("CalculatorApp");
         frame.setContentPane(myGUI.Home);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -55,64 +61,15 @@ public class CalculatorApp {
         noCapitals.put("the", 1);
     }
 
-    private String removeSpacePadding(String input) {
-        //this function takes an input string, checks for space at beginning and end,
-        //and if it exists removes that padding to trim to the "real" input
-        String edited = input;
-        boolean spaceAtEnd = ( edited.charAt(edited.length() - 1) == ' ' );
-        boolean spaceAtBegin = ( edited.charAt(0) == ' ' );
-
-        while(spaceAtBegin || spaceAtEnd) {
-            //check for only space at begin
-            if(spaceAtBegin && !spaceAtEnd) edited = edited.substring(1);
-            //check for only space at end
-            else if(spaceAtEnd && !spaceAtBegin) edited = edited.substring(0, input.length() - 1);
-            //check for space at beginning AND end
-            else if(spaceAtBegin && spaceAtEnd) edited = edited.substring(1, input.length() - 1);
-
-            //reset flags to see if there's still spaces
-            spaceAtEnd = ( edited.charAt(input.length() - 1) == ' ' );
-            spaceAtBegin = ( edited.charAt(0) == ' ' );
-        }
-
-        return edited;
-    }
-
-    private String capitalizeTitle(String input) {
-        //function that correctly capitalizes a title (input) and returns it
-        String[] separatedBySpace = input.split(" ");
-
-        //loop through each word,
-        //check if word is article/conjunction
-        //capitalize first letter if not
-        boolean noCapitalize = false;
-        String observed;
-        String output = "";
-
-        for(int i = 0; i < separatedBySpace.length; i++) {
-            observed = separatedBySpace[i].toLowerCase(Locale.ROOT);
-            noCapitalize = (i != 0) && (i != separatedBySpace.length - 1)
-                    && (noCapitals.containsKey(observed) || observed.length() <= 4);
-
-            //if we NEED to capitalize, capitalize the first letter
-            if(!noCapitalize) observed = Character.toUpperCase(observed.charAt(0)) + observed.substring(1);
-
-            //add to output
-            output += observed;
-
-            //if we aren't looking at the last index add a space to separate each word in title
-            if(i != separatedBySpace.length - 1) output += " ";
-        }
-
-        return output;
-    }
-
     private String removeSubstring(String regex, String input) {
+        //if no contain regex just return
+        if(!input.contains(regex)) return input;
+
         //remove any instances of the specified regex and return a string without those
         String output = "";
 
-        String[] parsed = input.split(regex);
-        for(int i = 0; i < parsed.length; i++) output += parsed[i];
+        String[] parsed = input.split(regex, 0);
+        for (String s : parsed) output += s;
 
         return output;
     }
@@ -122,30 +79,110 @@ public class CalculatorApp {
         String processing = "";
         boolean errorFound = false;
 
-        //get rid of percentages and decimals, if those exist
+        //get rid of percentages, if exist
         processing = removeSubstring("%", input);
-        processing = removeSubstring(".", processing);
+        System.out.println("Processing = " + processing);
+      //  processing = removeSubstring("\\.", processing);
+      //  System.out.println("Processing = " + processing);
 
         try {
-            output = Integer.parseInt(processing);
+            output = (int) Double.parseDouble(processing);
         } catch(NumberFormatException e) {
-            JOptionPane.showMessageDialog(frame, "Invalid percentage input. Please input your value in the format \"XX%\".",
+            JOptionPane.showMessageDialog(frame, "Invalid percentage input: " + processing.toUpperCase() + ". Please input your value as an integer in the format \"XX%\".",
                     "Percentage Parse Error", JOptionPane.ERROR_MESSAGE);
             errorFound = true;
         }
 
         if(output < 0 && !errorFound) {
-            JOptionPane.showMessageDialog(frame, "Invalid percentage input (negative input). Please input your value in the format \"XX%\".",
+            JOptionPane.showMessageDialog(frame, "Invalid percentage input (negative input). Please input your value as an integer in the format \"XX%\".",
                     "Percentage Input Error", JOptionPane.ERROR_MESSAGE);
             errorFound = true;
         } else if(output > 100 && !errorFound) {
-            JOptionPane.showMessageDialog(frame, "Invalid percentage input (input over 100%). Please input your value in the format \"XX%\".",
+            JOptionPane.showMessageDialog(frame, "Invalid percentage input (input over 100%). Please input your value as an integer in the format \"XX%\".",
                     "Percentage Input Error", JOptionPane.ERROR_MESSAGE);
             errorFound = true;
         }
 
         if(errorFound) return -1;
         return output;
+    }
+
+    private void addComboItem(JComboBox list, String key, String val) {
+        catList.addItem(new ComboItem(key, val));
+    }
+
+    private int searchForCat(String name, int weight) {
+        boolean nameFlag = false;
+        boolean weightFlag = false;
+        int index = 0;
+
+        for(int i = 0; i < inputCategories.size() && !nameFlag && !weightFlag; i++) {
+            //name must == searched name and weight must == searched weight
+            if(inputCategories.get(i).getName().equalsIgnoreCase(name)) {
+                nameFlag = true;
+                if(inputCategories.get(i).getWeight() == weight) weightFlag = true;
+
+                index = i;
+            }
+        }
+
+        if(nameFlag && weightFlag) return 2; //already in there
+        else if(nameFlag) return index; //override weight in this case
+
+        return -1; //nothing found
+    }
+
+    private int searchForCatName(String name) {
+        int index = -1;
+
+        for(int i = 0; i < inputCategories.size(); i++) {
+            //name must == searched name and weight must == searched weight
+            if(inputCategories.get(i).getName().equalsIgnoreCase(name)) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    private void resetCatList() {
+        catList.removeAllItems();
+        for(int i = 0; i < inputCategories.size(); i++) {
+            String name = inputCategories.get(i).getName();
+            addComboItem(catList, name, name);
+        }
+    }
+
+    private void appendToLabel(JLabel label, String toAppend) {
+        String temp = label.getText();
+        if(toAppend.charAt(0) != ' ') temp += (" " + toAppend);
+        else temp += toAppend;
+
+        label.setText(temp);
+    }
+
+    private void resetTotalWeightLabel() {
+        totalWeight.setText("Total Category Weight:");
+    }
+
+    private void resetDisplayLabels() {
+        catSelected.setText("Selected Category:");
+        weightSelected.setText("Weight (percent):");
+    }
+
+    private int getWeightSum() {
+        int sum = 0;
+        for(int i = 0; i < inputCategories.size(); i++) {
+            sum += inputCategories.get(i).getWeight();
+        }
+
+        return sum;
+    }
+
+    private boolean checkCanAdd(int weight) {
+        int sum = getWeightSum();
+        return sum + weight <= 100;
     }
 
     public CalculatorApp() {
@@ -161,8 +198,12 @@ public class CalculatorApp {
             public void actionPerformed(ActionEvent e) {
                 //get the category from the input box, trim padding, and fix caps
                 String category = catTitle.getText().toLowerCase(Locale.ROOT);
-                category = removeSpacePadding(category);
-                category = capitalizeTitle(category);
+
+                //create cat and set name/make adjustments to name
+                Category cat = new Category();
+                cat.setName(category);
+                cat.removeSpacePadding();
+                cat.capitalizeTitle(noCapitals);
 
                 //get percentage weight, trim padding, check if right format
                 String percentInput = weightInput.getText().toLowerCase(Locale.ROOT);
@@ -171,7 +212,76 @@ public class CalculatorApp {
                 //do not want to populate combo box if encounter error in parsing int
                 if(percent == -1) return;
 
-                //populate catList combo box if we've encountered no errors so far
+                //if no error in parsing int we can set weight
+                cat.setWeight(percent);
+
+                int searchState = searchForCat(cat.getName(), cat.getWeight());
+
+                //populate catList combo box/arraylist if we've encountered no errors so far
+                if(checkCanAdd(percent)) {
+                    if (searchState == -1) {
+                        inputCategories.add(cat);
+                        addComboItem(catList, cat.getName(), cat.getName());
+                    } else if (searchState != 2) {
+                        //if we have found the name but with a different weight override that one
+                        inputCategories.get(searchState).setWeight(percent);
+                    }
+
+                    resetTotalWeightLabel();
+                    appendToLabel(totalWeight, Integer.toString(getWeightSum()) + "/100%");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Invalid percentage input. This percent would make the total weight over 100% (with this addition, the sum would be " + Integer.toString(getWeightSum() + percent) + "%).",
+                            "Percentage Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        });
+        deleteCat.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object temp = catList.getSelectedItem();
+                String name = "";
+                boolean noErrors = true;
+
+                if (temp != null) {
+                    name = ((ComboItem)temp).getValue();
+                } else return;
+
+                if(noErrors) {
+                    //if no errors, proceed to delete category and reset combo box
+                    //to not have that category
+                    int i = searchForCatName(name);
+                    if(i >= 0) inputCategories.remove(i);
+                    resetTotalWeightLabel();
+                    appendToLabel(totalWeight, Integer.toString(getWeightSum()) + "/100%");
+                    resetCatList();
+                }
+            }
+        });
+        catList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object temp = catList.getSelectedItem();
+                String name = "";
+                boolean noErrors = true;
+
+                if (temp != null) {
+                    name = ((ComboItem)temp).getValue();
+                } else return;
+
+                int i = searchForCatName(name);
+                if(i >= 0) {
+                    resetDisplayLabels();
+                    appendToLabel(catSelected, inputCategories.get(i).getName());
+                    appendToLabel(weightSelected, Integer.toString(inputCategories.get(i).getWeight()) + "%");
+                }
+            }
+        });
+        contToInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) Home.getLayout();
+                cl.show(Home, "gradeInput");
             }
         });
     }
--- src/Category.java
+++ src/Category.java
@@ -1,2 +1,93 @@
+import java.util.HashMap;
+import java.util.Locale;
+
 public class Category {
+    private int weight;
+    private String name;
+
+    public Category() {
+        name = "";
+        weight = 0;
+    }
+
+    public Category(String title, int weight) {
+        this.name = title;
+        this.weight = weight;
+    }
+
+    public void setName(String input) {
+        name = input;
+    }
+    public void setWeight(int input) {
+        weight = input;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    public void parseName(String regex) {
+        //remove any instances of the specified regex and sets name to string without that regex in there
+        String output = "";
+
+        String[] parsed = name.split(regex);
+        for(int i = 0; i < parsed.length; i++) output += parsed[i];
+
+        name = output;
+    }
+
+    public void removeSpacePadding() {
+        //this function takes the name, checks for space at beginning and end,
+        //and if it exists removes that padding to trim to the "real" input
+        String edited = name;
+        boolean spaceAtEnd = ( edited.charAt(edited.length() - 1) == ' ' );
+        boolean spaceAtBegin = ( edited.charAt(0) == ' ' );
+
+        while(spaceAtBegin || spaceAtEnd) {
+            //check for only space at begin
+            if(spaceAtBegin && !spaceAtEnd) edited = edited.substring(1);
+                //check for only space at end
+            else if(spaceAtEnd && !spaceAtBegin) edited = edited.substring(0, name.length() - 1);
+                //check for space at beginning AND end
+            else if(spaceAtBegin && spaceAtEnd) edited = edited.substring(1, name.length() - 1);
+
+            //reset flags to see if there's still spaces
+            spaceAtEnd = ( edited.charAt(name.length() - 1) == ' ' );
+            spaceAtBegin = ( edited.charAt(0) == ' ' );
+        }
+
+        this.name = edited;
+    }
+
+    public void capitalizeTitle(HashMap<String, Integer> noCapitals) {
+        //function that correctly capitalizes a category's name
+        String[] separatedBySpace = name.split(" ");
+
+        //loop through each word,
+        //check if word is article/conjunction
+        //capitalize first letter if not
+        boolean noCapitalize = false;
+        String observed;
+        String output = "";
+
+        for(int i = 0; i < separatedBySpace.length; i++) {
+            observed = separatedBySpace[i].toLowerCase(Locale.ROOT);
+            noCapitalize = (i != 0) && (i != separatedBySpace.length - 1)
+                    && (noCapitals.containsKey(observed) || observed.length() <= 4);
+
+            //if we NEED to capitalize, capitalize the first letter
+            if(!noCapitalize) observed = Character.toUpperCase(observed.charAt(0)) + observed.substring(1);
+
+            //add to output
+            output += observed;
+
+            //if we aren't looking at the last index add a space to separate each word in title
+            if(i != separatedBySpace.length - 1) output += " ";
+        }
+
+        name = output;
+    }
+
 }
